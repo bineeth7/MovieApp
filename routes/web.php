3d0209a2453a956651373ef9cd6ace20e10b780e
Bineeth B C <bineethbcr7007@gmail.com>
2023-09-16 11:01:40 +0530

    Target class [MoviesController] does not exist.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/','MoviesController@index')->name('movies.index');
+Route::get('/movies/{movie}','MoviesController@show')->name('movies.show');
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
+// Route::view('/', 'index');
+// Route::view('/movie', 'show');
