662ec0fdb8c070e3a1372c5378a3bdcae1a33652
Bineeth B C <bineethbcr7007@gmail.com>
2023-09-18 22:45:24 +0530

    main

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
-Route::get('/','MoviesController@index')->name('movies.index');
-Route::get('/movies/{movie}','MoviesController@show')->name('movies.show');
+Route::get('/movie/{id}', [MovieController::class, 'getMovieDetails']);
 
-// Route::view('/', 'index');
-// Route::view('/movie', 'show');
+Route::view('/', 'index');
+Route::view('/movie', 'show');
