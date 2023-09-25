4dafc2f9cfac94fe82398f8dfcd2b0d8c8c91968
Bineeth B C <bineethbcr7007@gmail.com>
2023-09-25 18:59:04 +0530

    Popular Movies & Now Playing Components added

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -23,7 +23,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-7">
             <!-- start movie loop-->
             @foreach($nowPlayingMovies as $movie)
-                <!-- <x-movie-card :movie="$movie" :genres="$genres"/> -->
+                <x-movie-card :movie="$movie" :genres="$genres"/>
             @endforeach
             <!-- end movie loop-->
             </div>
