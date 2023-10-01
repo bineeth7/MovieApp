18b6cd093ae412a940e9a74825789d296189222f
Bineeth B C <bineethbcr7007@gmail.com>
2023-10-01 14:29:21 +0530

    hiding api and test pass

--- tests/Feature/ViewMoviesTest.php
+++ tests/Feature/ViewMoviesTest.php
@@ -7,14 +7,12 @@ use Tests\TestCase;
 
 class ViewMoviesTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_main_page_shows_correct_info()
     {
         $response = $this->get(route('movies.index'));
-        $response->assertSuccessful();
 
-        //$response->assertStatus(200);
+        $response->assertSuccessful();
+        $response->assertSee('Popular Movies');
     }
+
 }
