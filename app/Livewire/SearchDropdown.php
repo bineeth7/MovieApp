c82ffccf1006cfed024b73705abff2075d9e6ac9
Bineeth B C <bineethbcr7007@gmail.com>
2023-10-06 23:03:22 +0530

    query movie search added but showing invalid api key

--- app/Livewire/SearchDropdown.php
+++ app/Livewire/SearchDropdown.php
@@ -3,14 +3,33 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class SearchDropdown extends Component
 {
-    public $search = 'he';
+    public $search = 'Avengers';
 
     public function render()
     {
-        return view('livewire.search-dropdown');
+        $apiKey = env('TMDB_API_KEY');
+        $searchResults = [];
+        $searchResults = Http::withToken($apiKey)
+            ->get('https://api.themoviedb.org/3/search/movie?query=' . $this->search)
+            ->json()['results'];
+
+            dump($searchResults);
+        
+
+        return view('livewire.search-dropdown', [
+            'searchResults' => $searchResults,
+        ]);
     }
 }
+
+
+// $searchResults = Http::withToken($apiKey)
+
+        //$this->searchResults = $searchResults;
+        //$searchResults = Http::get('https://api.themoviedb.org/3/search/movie?query=' .$this->search . '?api_key=' .$apiKey)->json();
+        
