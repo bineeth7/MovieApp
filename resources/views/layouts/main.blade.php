b98c6e77b43df708a150e4068a2fe4a139ae8726
Bineeth B C <bineethbcr7007@gmail.com>
2023-10-16 13:58:58 +0530

    MovieAppIcon.png added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie App</title>
+    <link rel="icon" href="/images/MovieAppIcon.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     @livewireStyles
