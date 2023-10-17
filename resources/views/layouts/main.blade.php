f4d8d177dad1134f1c4513b9e32724121a553cc2
Bineeth B C <bineethbcr7007@gmail.com>
2023-10-17 19:49:32 +0530

    Alphine dropdown isOpen, @click.away, @focus, @keydown.escape.window added to hide and show the dropdown

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     @livewireStyles
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="font-sans bg-black text-white">
